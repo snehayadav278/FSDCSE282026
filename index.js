257aebbca060a9b73325bc2c461f7de796c030d0
snehayadav278 <snehay851@gmail.com>
2026-07-27 15:43:51 +0530

    var vs let

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello , using JS");
+// console.log("Hello , using JS");
+
+// function sum(a,b){
+//     let add = a+b;
+//     let result = Math.sqrt(add);
+//     console.log(result);
+// }
+// sum(9, 16);
+
+
+// var a=34;
+// if(a>20){
+//     var a=45;
+//     console.log("a inside a = " + a);
+// }
+// console.log("a outside a = "+ a);
+
+let a=34;
+if(a>20){
+    let a=45;
+    console.log("a inside a = " + a);
+}
+console.log("a outside a = "+ a);
+
