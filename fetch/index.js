1415f900ccc495d1c7078fea8e8db58af61d6cf3
snehayadav278 <snehay851@gmail.com>
2026-08-24 15:44:11 +0530

    fetch api

---
+++ fetch/index.js
@@ -0,0 +1,19 @@
+// console.log("hello");
+const root = document.getElementById('container');
+console.log(root);
+
+const button = document.getElementById('btn');
+console.log(button);
+
+
+async function getData(){
+    // alert("hiii");
+    const serverData = await fetch('https://fakestoreapi.com/products');
+    const jsonData = await serverData.json();
+    // console.log(jsonData[0].title);
+
+    root.innerHTML = `<h2 style="color:red">${jsonData[0].title}</h2>`;
+    
+}
+
+button.addEventListener('click' , getData());
